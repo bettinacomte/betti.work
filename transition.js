07eeec703a84e0b8ad043ea1d7436bc123e438d4
Bettina Comte <bettina.comte@gmail.com>
2018-10-15 23:12:02 +0200

    Animating transition slide between Project page and News page

--- transition.js
+++ transition.js
@@ -1,14 +1,11 @@
 $(document).ready(function(){
     $("#news").click(function(){
-        console.log("toto")
+        console.log("toto");
         $("#projectspage").animate({
-          x: "-100vw"
-        }, 5000);
-        $("#test2").animate({
-            left: '250px',
-            opacity: '0.5',
-            height: '250px',
-            width: '250px'
-        });
+          left: '-100vw',
+        }, 1000);
+        $("#newspage").animate({
+          right: '100vw',
+        }, 1000);
     });
 });
