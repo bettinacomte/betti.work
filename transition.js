9a6d8e9aa3359c00da9ccc4c5f5988f161835502
Bettina Comte <bettina.comte@gmail.com>
2018-10-17 00:03:30 +0200

    Slide animation between pages complited

--- transition.js
+++ transition.js
@@ -1,14 +1,29 @@
  $(document).ready(function(){
-    $("#news").click(function(){
+    $("#news").click(function goNews(){
         console.log("toto");
-        $("#projectspage").animate({
-          left: '-100vw',
-        }, 1000);
-        $("#newspage").animate({
-          right: '100vw',
-        }, 1000);
+        document.getElementById("newspage").style.transform = "translateX(-100vw)";
+        document.getElementById("newspage").style.transitionDuration = "1s";
+        document.getElementById("projectspage").style.transform = "translateX(-100vw)";
+        document.getElementById("projectspage").style.transitionDuration = "1s";
+        document.getElementById("infopage").style.transform = "translateX(-100vw)";
+        document.getElementById("infopage").style.transitionDuration = "1s";
+    });
+    $("#projects").click(function goProjects(){
+        console.log("toto2");
+        document.getElementById("newspage").style.transform = "translateX(0)";
+        document.getElementById("newspage").style.transitionDuration = "1s";
+        document.getElementById("projectspage").style.transform = "translateX(0)";
+        document.getElementById("projectspage").style.transitionDuration = "1s";
+        document.getElementById("infopage").style.transform = "translateX(0)";
+        document.getElementById("infopage").style.transitionDuration = "1s";
+    });
+    $("#info").click(function goInfos(){
+        console.log("toto3");
+        document.getElementById("newspage").style.transform = "translateX(-200vw)";
+        document.getElementById("newspage").style.transitionDuration = "1s";
+        document.getElementById("projectspage").style.transform = "translateX(-200vw)";
+        document.getElementById("projectspage").style.transitionDuration = "1s";
+        document.getElementById("infopage").style.transform = "translateX(-200vw)";
+        document.getElementById("infopage").style.transitionDuration = "1s";
     });
 });
-
-var links = document.querySelectorAll(".headerlink");
-var section = document.querySelectorAll("sections");
