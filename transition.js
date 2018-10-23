43f63e4550a59422c89f1e0ef795d4020f92fc86
Bettina Comte <bettina.comte@gmail.com>
2018-10-23 23:53:09 +0200

    underbar transition on header links

--- transition.js
+++ transition.js
@@ -2,31 +2,37 @@
     $("#news").click(function goNews(){
         console.log("toto");
         document.getElementById("newspage").style.transform = "translateX(-100vw)";
-        document.getElementById("newspage").style.transitionDuration = "1s";
+        document.getElementById("newspage").style.transitionDuration = "700ms";
         document.getElementById("projectspage").style.transform = "translateX(-100vw)";
-        document.getElementById("projectspage").style.transitionDuration = "1s";
+        document.getElementById("projectspage").style.transitionDuration = "700ms";
         document.getElementById("infopage").style.transform = "translateX(-100vw)";
-        document.getElementById("infopage").style.transitionDuration = "1s";
+        document.getElementById("infopage").style.transitionDuration = "700ms";
 
-        document.getElementById("underbar").style.transform = "translateX(+200px)";
-        document.getElementById("underbar").style.transitionDuration = "1s";
+        document.getElementById("underbar").style.transform = "translateX(+160px)";
+        document.getElementById("underbar").style.transitionDuration = "700ms";
     });
     $("#projects").click(function goProjects(){
         console.log("toto2");
         document.getElementById("newspage").style.transform = "translateX(0)";
-        document.getElementById("newspage").style.transitionDuration = "1s";
+        document.getElementById("newspage").style.transitionDuration = "700ms";
         document.getElementById("projectspage").style.transform = "translateX(0)";
-        document.getElementById("projectspage").style.transitionDuration = "1s";
+        document.getElementById("projectspage").style.transitionDuration = "700ms";
         document.getElementById("infopage").style.transform = "translateX(0)";
-        document.getElementById("infopage").style.transitionDuration = "1s";
+        document.getElementById("infopage").style.transitionDuration = "700ms";
+
+        document.getElementById("underbar").style.transform = "translateX(0px)";
+        document.getElementById("underbar").style.transitionDuration = "700ms";
     });
     $("#info").click(function goInfos(){
         console.log("toto3");
         document.getElementById("newspage").style.transform = "translateX(-200vw)";
-        document.getElementById("newspage").style.transitionDuration = "1s";
+        document.getElementById("newspage").style.transitionDuration = "700ms";
         document.getElementById("projectspage").style.transform = "translateX(-200vw)";
-        document.getElementById("projectspage").style.transitionDuration = "1s";
+        document.getElementById("projectspage").style.transitionDuration = "700ms";
         document.getElementById("infopage").style.transform = "translateX(-200vw)";
-        document.getElementById("infopage").style.transitionDuration = "1s";
+        document.getElementById("infopage").style.transitionDuration = "700ms";
+
+        document.getElementById("underbar").style.transform = "translateX(+298px)";
+        document.getElementById("underbar").style.transitionDuration = "700ms";
     });
 });
