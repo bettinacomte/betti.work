f1edaa96880f5a4562f199fd4b2b5a0300d8efc0
Bettina Comte <bettina.comte@gmail.com>
2018-10-24 10:47:10 +0200

    Adding ease in out on transitions

--- transition.js
+++ transition.js
@@ -2,37 +2,37 @@
     $("#news").click(function goNews(){
         console.log("toto");
         document.getElementById("newspage").style.transform = "translateX(-100vw)";
-        document.getElementById("newspage").style.transitionDuration = "800ms";
+        document.getElementById("newspage").style.transition = "800ms ease-in-out";
         document.getElementById("projectspage").style.transform = "translateX(-100vw)";
-        document.getElementById("projectspage").style.transitionDuration = "800ms";
+        document.getElementById("projectspage").style.transition = "800ms ease-in-out";
         document.getElementById("infopage").style.transform = "translateX(-100vw)";
-        document.getElementById("infopage").style.transitionDuration = "800ms";
+        document.getElementById("infopage").style.transition = "800ms ease-in-out";
 
         document.getElementById("underbar").style.transform = "translateX(+160px)";
-        document.getElementById("underbar").style.transitionDuration = "400ms";
+        document.getElementById("underbar").style.transition = "400ms ease-in-out";
     });
     $("#projects").click(function goProjects(){
         console.log("toto2");
         document.getElementById("newspage").style.transform = "translateX(0)";
-        document.getElementById("newspage").style.transitionDuration = "800ms";
+        document.getElementById("newspage").style.transition = "800ms ease-in-out";
         document.getElementById("projectspage").style.transform = "translateX(0)";
-        document.getElementById("projectspage").style.transitionDuration = "800ms";
+        document.getElementById("projectspage").style.transition = "800ms ease-in-out";
         document.getElementById("infopage").style.transform = "translateX(0)";
-        document.getElementById("infopage").style.transitionDuration = "800ms";
+        document.getElementById("infopage").style.transition = "800ms ease-in-out";
 
         document.getElementById("underbar").style.transform = "translateX(0px)";
-        document.getElementById("underbar").style.transitionDuration = "400ms";
+        document.getElementById("underbar").style.transition = "400ms ease-in-out";
     });
     $("#info").click(function goInfos(){
         console.log("toto3");
         document.getElementById("newspage").style.transform = "translateX(-200vw)";
-        document.getElementById("newspage").style.transitionDuration = "800ms";
+        document.getElementById("newspage").style.transition = "800ms ease-in-out";
         document.getElementById("projectspage").style.transform = "translateX(-200vw)";
-        document.getElementById("projectspage").style.transitionDuration = "800ms";
+        document.getElementById("projectspage").style.transition = "800ms ease-in-out";
         document.getElementById("infopage").style.transform = "translateX(-200vw)";
-        document.getElementById("infopage").style.transitionDuration = "800ms";
+        document.getElementById("infopage").style.transition = "800ms ease-in-out";
 
         document.getElementById("underbar").style.transform = "translateX(+298px)";
-        document.getElementById("underbar").style.transitionDuration = "400ms";
+        document.getElementById("underbar").style.transition = "400ms ease-in-out";
     });
 });
